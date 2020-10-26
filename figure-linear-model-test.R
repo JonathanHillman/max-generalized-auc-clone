0c5e51223af9dd6ebc35432f37069100078ee4a1
Jonathan Hillman <jonathandalehillman@gmail.com>
2020-10-25 20:17:21 -0700

    Update figure-linear-model-test.R

--- figure-linear-model-test.R
+++ figure-linear-model-test.R
@@ -1,36 +1,15 @@
 library(data.table)
 library(ggplot2)
+library(dplyr)
 
-auc.improved <- readRDS("auc.improved.rds")
-roc.dt.list <- list()
-
-for(test.fold.i in 1:nrow(auc.improved)){
-  one.fold <- auc.improved[test.fold.i]
-  roc.dt.list[[test.fold.i]] <- one.fold[, data.table(
-    data.name=set.name, test.fold=fold, pred.name,
-    rows=.N,
-    roc[[1]])]
-}
-
-(roc.dt <- do.call(rbind, roc.dt.list))
-roc.dt[, fn0 := fn-min(fn), by=.(data.name, test.fold, pred.name)]
-roc.dt[, min.fp.fn := ifelse(fp<fn0, fp, fn0)]
-roc.dt[, width := max.thresh-min.thresh]
-roc.dt[, area := ifelse(min.fp.fn==0, 0, min.fp.fn*width)]
-
-(aum.dt <- roc.dt[, .(
-  aum=sum(area)
-), keyby=.(data.name, test.fold, pred.name)])
-best.aum <- aum.dt[, .SD[which.min(aum), .(best.aum=aum)], by=.(data.name, test.fold)]
-
-
-#testFold.vec <- Sys.glob("../neuroblastoma-data/data/*/cv/*/testFolds/*")
-testFold.vec <- Sys.glob("~/ROC-Curve-Analysis/neuroblastoma-data-master/neuroblastoma-data-master/data/*/cv/*/testFolds/*")
-testFold.vec <- file.path("C:/Users/jonat/Documents/ROC-Curve-Analysis/neuroblastoma-data-master/neuroblastoma-data-master/data/ATAC_JV_adipose/cv/equal_labels/testFolds/4")
-testFold.path <- testFold.vec
+testFold.vec <- Sys.glob("neuroblastoma-data/data/*/cv/*/testFolds/*")
+testFold.vec <- Sys.glob(one.testFold.path)
+one.testFold.path <- file.path("C:/Users/jonat/Documents/ROC-Curve-Analysis/neuroblastoma-data/data/ATAC_JV_adipose/cv/equal_labels/testFolds/4")
+testFold.path <- one.testFold.path
 
 #OneFold <- function(testFold.path)
 {
+  #Filename and directory initializations
   cv.path <- dirname(dirname(testFold.path))
   folds.csv <- file.path(cv.path, "folds.csv")
   cv.type <- basename(cv.path)
@@ -38,6 +17,9 @@ testFold.path <- testFold.vec
   data.dir <- dirname(dirname(cv.path))
   data.name <- basename(data.dir)
   data.list <- list()
+  
+  
+  #Read the "inputs.csv", "outputs.csv", and "evaluations.csv", for each data 
   for(f in c("inputs", "outputs", "evaluation")){
     f.csv.xz <- file.path(data.dir, paste0(f, ".csv.xz"))
     if(file.exists(f.csv.xz)){
@@ -47,14 +29,28 @@ testFold.path <- testFold.vec
     f.dt <- data.table::fread(f.csv)
     data.list[[f]] <- f.dt
   }
+  
+  
   ## replace positive fp/fn at end with 0 to avoid AUM=Inf.
   data.list[["evaluation"]][min.log.lambda==-Inf & 0<fn, fn := 0]
   data.list[["evaluation"]][max.log.lambda==Inf & 0<fp, fp := 0]
-  ## read folds.  
+  
+  
+  ## read folds.csv for the specific data type
   folds.dt <- data.table::fread(folds.csv)
+  #initialize my own validation folds
+  n.val.folds <- 4
+  cv.folds <- 1:n.val.folds
+  #set test folds 
   folds.dt[fold == test.fold, set := "test"]
-  folds.dt[fold != test.fold, set := rep(
-    1:4, l=.N)]
+  folds.dt[fold != test.fold, set:= "subtrain"]
+  val.fold.assignments <- rep(cv.folds, l=nrow(folds.dt[fold != test.fold]))
+  
+  iteration.dt.list <- list()
+  for(val.fold in cv.folds)
+  {
+  folds.dt[set != "test",] <- folds.dt[set != "test",] %>% 
+    mutate(set = ifelse(val.fold.assignments == val.fold, "validation", "subtrain"))
   seqs.train <- folds.dt[["sequenceID"]]
   X.all <- scale(data.list$inputs[, -1])
   rownames(X.all) <- data.list$inputs$sequenceID
@@ -64,27 +60,22 @@ testFold.path <- testFold.vec
     set.list[[s]] <- rownames(X.finite) %in% folds.dt[s==set, sequenceID]
   }
   X.list <- lapply(set.list, function(i)X.finite[i, ])
-  neg.t.X.subtrain.list <- lapply(X.list[1:4], function(x){-t(x)})
+  neg.t.X.subtrain <- -t(X.list[["subtrain"]])
   y.train <- data.list[["outputs"]][
     seqs.train,
     cbind(min.log.lambda, max.log.lambda),
     on="sequenceID"]
   keep <- apply(is.finite(y.train), 1, any)
   X.train <- X.finite[seqs.train, ]
-  
   init.fun.list <- list(
     IntervalRegressionCV=function(){
       fit <- penaltyLearning::IntervalRegressionCV(
         X.train[keep, ],
         y.train[keep, ])  
       fit[["param.mat"]]
-    },
-    zero=function(){
-      N.param <- ncol(X.finite)+1
-      rep(0, N.param)+rnorm(N.param)
     }
   )
-  iteration.dt.list <- list()
+  
   for(seed in 1:4)for(init.name in names(init.fun.list)){
     init.fun <- init.fun.list[[init.name]]
     set.seed(seed)
@@ -99,14 +90,12 @@ testFold.path <- testFold.vec
       set.dt <- pred.dt[is.set]
       penaltyLearning::ROChange(
         data.list$evaluation, set.dt, "sequenceID")
-      
     }
-    neg.t.X.subtrain <- neg.t.X.subtrain.list[[seed]]
     for(iteration in 1:50){
       summary.dt.list <- list()
       set.roc.list <- list()
       for(set in names(set.list)){
-        set.roc.list[[set]] <- computeAUM(weight.vec, intercept[seed], set.list[[set]])
+        set.roc.list[[set]] <- computeAUM(weight.vec, intercept, set.list[[set]])
         summary.dt.list[[set]] <- with(set.roc.list[[set]], data.table(
           set,
           thresholds[threshold=="predicted"],
@@ -114,11 +103,11 @@ testFold.path <- testFold.vec
           aum))
       }
       summary.dt <- do.call(rbind, summary.dt.list)
-      iteration.dt.list[[paste(seed, init.name, iteration)]] <- data.table(
-        seed, init.name, iteration, summary.dt)
+      iteration.dt.list[[paste(seed, init.name, iteration, val.fold)]] <- data.table(
+        seed, init.name, iteration, paste(val.fold), summary.dt)
       cat(sprintf(
-        "it=%d seed=%d init=%s\n",
-        iteration, seed, init.name))
+        "it=%d seed=%d init=%s val=%d\n",
+        iteration, seed, init.name, val.fold))
       g.dt <- set.roc.list[["subtrain"]][["aum.grad"]]
       ## If aum.grad has some problems with no changes in error then
       ## they may be missing.
@@ -131,41 +120,157 @@ testFold.path <- testFold.vec
       take.step <- function(s){
         weight.vec + s*direction.vec
       }
+      
+      #line search
       set.aum.list <- list()
       for(step.size in 10^seq(-10, 0, by=0.5)){
         new.weight.vec <- take.step(step.size)
-        for(set in 1:4){
+        for(set in "subtrain"){
           set.roc <- computeAUM(new.weight.vec, 0, set.list[[set]])
           set.aum.list[[paste(step.size, set)]] <- data.table(
             step.size, set, aum=set.roc$aum,
             intercept=set.roc$thresholds[
               threshold=="min.error", (max.thresh+min.thresh)/2])
-        }
+        }#line search
       }
       set.aum <- do.call(rbind, set.aum.list)
-      best.dt <- set.aum[, .SD[which.min(aum)], by=set]
-      ggplot()+
-        geom_line(aes(
-          step.size, aum),
-          data=set.aum)+
-        geom_point(aes(
-          step.size, aum),
-          data=best.dt)+
-        geom_text(aes(
-          step.size, aum, label=aum),
-          vjust=0,
-          data=best.dt)+
-        scale_x_log10()+
-        scale_y_log10()+
-        facet_grid(set ~ ., scales="free")
-      weight.vec <- take.step(best.dt[set == seed, step.size])
+      best.dt <- set.aum[, .SD[min(aum)==aum], by=set]
+      weight.vec <- take.step(best.dt[["step.size"]])
       intercept <- best.dt[["intercept"]]
     }#iteration
   }#seed/init.name
-  data.table(
+  
+  }#validation fold
+  cv.dt <- data.table(
     do.call(rbind, iteration.dt.list),
     data.name, cv.type, test.fold)
+  
+  mean.dt <- cv.dt[, .(mean.aum=mean(aum)), by=.(seed, iteration, set)]
+  val.cv.dt <- cv.dt[set == "validation"]
+  val.mean.dt <- mean.dt[set == "validation"]
+  val.min.dt <- val.mean.dt[, .(iteration = which.min(mean.aum), min.aum = min(mean.aum)), by=.(seed) ]
+  
+  ggplot(data = val.cv.dt) +
+    geom_line(mapping = aes(x = iteration, y = aum, color = val.fold)) +
+    geom_line(data = val.mean.dt, mapping = aes(x = iteration, y = mean.aum)) +
+    geom_point(data = val.min.dt, mapping = aes(x = iteration, y = min.aum)) +
+    facet_wrap(.~seed)
+  
+  selected.iter <- round(mean(val.min.dt$iteration))
+  
+  
+  folds.dt[fold == test.fold, set := "test"]
+  folds.dt[fold != test.fold, set:= "subtrain"]
+  iteration.dt.list <- list ()
+  iteration.dt.list <- list()
+
+    seqs.train <- folds.dt[["sequenceID"]]
+    X.all <- scale(data.list$inputs[, -1])
+    rownames(X.all) <- data.list$inputs$sequenceID
+    X.finite <- X.all[, apply(is.finite(X.all), 2, all)]
+    set.list <- list()
+    for(s in unique(folds.dt$set)){
+      set.list[[s]] <- rownames(X.finite) %in% folds.dt[s==set, sequenceID]
+    }
+    X.list <- lapply(set.list, function(i)X.finite[i, ])
+    neg.t.X.subtrain <- -t(X.list[["subtrain"]])
+    y.train <- data.list[["outputs"]][
+      seqs.train,
+      cbind(min.log.lambda, max.log.lambda),
+      on="sequenceID"]
+    keep <- apply(is.finite(y.train), 1, any)
+    X.train <- X.finite[seqs.train, ]
+    init.fun.list <- list(
+      IntervalRegressionCV=function(){
+        fit <- penaltyLearning::IntervalRegressionCV(
+          X.train[keep, ],
+          y.train[keep, ])  
+        fit[["param.mat"]]
+      }
+    )
+    
+    for(seed in 1:4)for(init.name in names(init.fun.list)){
+      init.fun <- init.fun.list[[init.name]]
+      set.seed(seed)
+      int.weights <- init.fun()
+      weight.vec <- int.weights[-1]
+      intercept <- int.weights[1]
+      computeAUM <- function(w, i, is.set){
+        pred.pen.vec <- (X.finite %*% w) + i
+        pred.dt <- data.table(
+          sequenceID=rownames(pred.pen.vec),
+          pred.log.lambda=as.numeric(pred.pen.vec))
+        set.dt <- pred.dt[is.set]
+        penaltyLearning::ROChange(
+          data.list$evaluation, set.dt, "sequenceID")
+      }
+      for(iteration in 1:selected.iter){
+        summary.dt.list <- list()
+        set.roc.list <- list()
+        for(set in names(set.list)){
+          set.roc.list[[set]] <- computeAUM(weight.vec, intercept, set.list[[set]])
+          summary.dt.list[[set]] <- with(set.roc.list[[set]], data.table(
+            set,
+            thresholds[threshold=="predicted"],
+            auc,
+            aum))
+        }
+        summary.dt <- do.call(rbind, summary.dt.list)
+        iteration.dt.list[[paste(seed, init.name, iteration)]] <- data.table(
+          seed, init.name, iteration, summary.dt)
+        cat(sprintf(
+          "it=%d seed=%d init=%s\n",
+          iteration, seed, init.name))
+        g.dt <- set.roc.list[["subtrain"]][["aum.grad"]]
+        ## If aum.grad has some problems with no changes in error then
+        ## they may be missing.
+        g.vec <- rep(0, ncol(neg.t.X.subtrain))
+        names(g.vec) <- colnames(neg.t.X.subtrain)
+        g.vec[
+          g.dt[["sequenceID"]]
+        ] <- g.dt[["lo"]]
+        direction.vec <- neg.t.X.subtrain %*% g.vec
+        take.step <- function(s){
+          weight.vec + s*direction.vec
+        }
+        
+        #line search
+        set.aum.list <- list()
+        for(step.size in 10^seq(-10, 0, by=0.5)){
+          new.weight.vec <- take.step(step.size)
+          for(set in "subtrain"){
+            set.roc <- computeAUM(new.weight.vec, 0, set.list[[set]])
+            set.aum.list[[paste(step.size, set)]] <- data.table(
+              step.size, set, aum=set.roc$aum,
+              intercept=set.roc$thresholds[
+                threshold=="min.error", (max.thresh+min.thresh)/2])
+          }#line search
+        }
+        set.aum <- do.call(rbind, set.aum.list)
+        best.dt <- set.aum[, .SD[min(aum)==aum], by=set]
+        weight.vec <- take.step(best.dt[["step.size"]])
+        intercept <- best.dt[["intercept"]]
+      }#iteration
+    }#seed/init.name
+  
+  post.cv.dt <- data.table(
+    do.call(rbind, iteration.dt.list),
+    data.name, cv.type, test.fold)
+  post.cv.dt$seed <- paste(post.cv.dt$seed)
+  
+  test.post.cv.dt <- post.cv.dt[set == "test"]
+  
+  best.linear.dt <- test.post.cv.dt[, .(aum = min(aum), type = "best.linear"), by = seed ]
+  selected.dt <- test.post.cv.dt[, .(aum = aum[selected.iter], type = "selected"), by = seed]
+  initial.dt <- test.post.cv.dt[, .(aum = aum[1], type = "initial"), by = seed]
+  
+  test.aum.dt <- rbind(best.linear.dt, selected.dt, initial.dt)
+  
+  ggplot(data = test.aum.dt) +
+    geom_point(aes(x = aum, y = type, color = seed))
+  
 }
+
 
 all.it.list <- list()
 for(testFold.i in seq_along(testFold.vec)){
@@ -186,38 +291,11 @@ subtrain.it <- all.it[set=="subtrain"]
 subtrain.it[, diff := c(NA, diff(aum)), by=.(init.name, data.name, test.fold, seed)]
 subtrain.it[, .(init.name, data.name, test.fold, iteration, aum, diff)]
 subtrain.it[diff>1e-6]
-gg <- ggplot()+
-  ggtitle("check if train AUM decreases")+
-  theme_bw()+
-  theme(panel.spacing=grid::unit(0, "lines"))+
-  geom_line(aes(
-    iteration, aum,
-    group=paste(seed, init.name)),
-    data=subtrain.it)+
-  facet_grid(init.name + data.name + test.fold ~ ., scales="free", labeller=label_both)
-png(
-  "figure-linear-model-test-aum-train-decreases.png",
-  width=4, height=35, res=100, units="in")
-print(gg)
-dev.off()
+
+
+
 
 validation.it <- all.it[set=="validation"]
-ggplot()+
-  theme_bw()+
-  theme(panel.spacing=grid::unit(0, "lines"))+
-  scale_y_log10()+
-  geom_line(aes(
-    iteration, aum, color=init.name,
-    group=paste(seed, init.name)),
-    data=validation.it)+
-  geom_point(aes(
-    iteration, aum, color=init.name,
-    group=paste(seed, init.name)),
-    data=validation.it[
-      ,
-      .SD[which.min(aum)],
-      by=.(data.name, test.fold, init.name, seed)])+
-  facet_grid(data.name + test.fold ~ ., scales="free", labeller=label_both)
 
 valid.best.ids <- all.it[
   set=="validation",
@@ -264,25 +342,7 @@ ifac <- function(x)factor(
   x, c("initial", "selected", "best.linear", "best.pred"))
 test.show[, Iterations := ifac(iterations)]
 best.pred[, Iterations := ifac("best.pred")]
-gg <- ggplot()+
-  ggtitle("Test AUM, selected=min valid aum, best=min test aum, max it=50")+
-  theme_bw()+
-  theme(panel.spacing=grid::unit(0, "lines"))+
-  geom_point(aes(
-    aum, Iterations, color=factor(test.fold)),
-    shape=1,
-    data=test.show)+
-  scale_y_discrete(drop=FALSE)+
-  geom_point(aes(
-    best.aum, Iterations, color=factor(test.fold)),
-    shape=1,
-    data=best.pred)+
-  facet_grid(init.name ~ data.name, scales="free", labeller=label_both)
-png(
-  "figure-linear-model-test-compare-init.png",
-  width=8, height=6, res=100, units="in")
-print(gg)
-dev.off()
+
 
 test.show[, neg.auc := -auc]
 test.show.tall <- melt(
@@ -291,30 +351,4 @@ test.show.tall <- melt(
 test.iCV <- dcast(
   test.show.tall,
   data.name + test.fold + variable + seed ~ iterations)
-gg <- ggplot()+
-  ggtitle("Test set metrics, init=IntervalRegressionCV, selected worse than initial")+
-  geom_point(aes(
-    initial, selected, color=factor(seed)),
-    shape=1,
-    data=test.iCV)+
-  geom_abline()+
-  facet_wrap(~ data.name + variable, scales="free", labeller=label_both, ncol=3)
-png(
-  "figure-linear-model-test-initial-selected.png",
-  width=10, height=6, res=100, units="in")
-print(gg)
-dev.off()
 
-gg <- ggplot()+
-  ggtitle("Test set metrics, init=IntervalRegressionCV, best about the same as initial")+
-  geom_point(aes(
-    initial, best.linear, color=factor(seed)),
-    shape=1,
-    data=test.iCV)+
-  geom_abline()+
-  facet_wrap(~ data.name + variable, scales="free", labeller=label_both, ncol=3)
-png(
-  "figure-linear-model-test-initial-best.png",
-  width=10, height=6, res=100, units="in")
-print(gg)
-dev.off()
